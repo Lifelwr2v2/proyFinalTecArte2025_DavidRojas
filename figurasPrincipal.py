39ef2cce682e185786f31fd9ec110decb4076d91
david <david.rojas.v2007@gmail.com>
2025-10-31 12:15:55 -0600

    mi primer commit

---
+++ figurasPrincipal.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+dataFile = pd.read_csv("figuras.csv")
+
+areas = []
+perimetros = []
+
+for index, row in dataFile.iterrows():
+	print(f"Fila {index}: Figura = {row['Figura']}, Medida 1 = {row['Medida1']}, Medida 2 = {row['Medida2']}")
+
+
+
